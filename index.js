93c302a5232c412987ce7a984a5f95f4bdef46b2
Eriberto Guzman <eriberto176@gmail.com>
2020-04-08 08:48:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ const app = "I don't do much."
 
 var dodger = document.getElementById('dodger')
 
+function moveDodgerLeft() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+ 
+  if (right > ) {
+    dodger.style.left = `${right + 1}px`
+  }
+}
